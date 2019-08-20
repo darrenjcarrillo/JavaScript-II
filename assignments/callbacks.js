b891cc105da92dcb021b5ef60c9863a1d2c57345
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-08-20 13:02:54 -0700

    done with the callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,27 +38,69 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
+// Number 1
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+const test1 = getLength(items, item => `the length is ${item}`);
+console.log(test1);
+
+// Number 2
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[3]);
 }
 
+const lastItem = last(items, item => `${item} is the last item`);
+
+console.log(lastItem);
+
+// Number 3
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
 
+const add = (x, y) => {
+  return x + y;
+};
+
+console.log(sumNums(3, 4, add));
+
+// Number 4
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
 
+const multiplied = (x, y) => {
+  return x * y;
+};
+
+console.log(multiplyNums(5, 5, multiplied));
+
+// Number 5
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+  if (list.includes(item)) {
+    // if the list does not include the this item
+    return cb(true); // passing callback if true
+  }
+  return cb(false); // passing callback if false
 }
+
+const check = contains(3, [1, 2, 3], match => {
+  if (match) {
+    return `item is inside the list`;
+  }
+  return `item is not inside the list`;
+});
+
+console.log(check);
 
 /* STRETCH PROBLEM */
 
